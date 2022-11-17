f2afba5f32b15405ad3f64791cf6c8151298469a
A01751272 <a01751272@itesm.mx>
2022-11-16 19:54:41 -0600

    Primera conexion
    Primera conexión con mesa, robots se mueven lento

--- ActividadIntegradora/Assets/Code/AgentController.cs
+++ ActividadIntegradora/Assets/Code/AgentController.cs
@@ -1,4 +1,4 @@
-// TC2008B. Sistemas Multiagentes y Gráficas Computacionales
+    // TC2008B. Sistemas Multiagentes y Grï¿½ficas Computacionales
 // C# client to interact with Python. Based on the code provided by Sergio Ruiz.
 // Equipo 4, Noviembre 2022
 
@@ -60,7 +60,6 @@ public class BoxesData
 
 public class AgentController : MonoBehaviour
 {
-
     // private string url = "https://agents.us-south.cf.appdomain.cloud/";
     string serverUrl = "http://localhost:8585";
     string getBoxesEndpoint = "/getBoxes";
@@ -92,21 +91,26 @@ public class AgentController : MonoBehaviour
         boxes = new Dictionary<string, GameObject>();
         robots = new Dictionary<string, GameObject>();
 
-        floor.transform.localScale = new Vector3((float)width / 10, 1, (float)height / 10);
-        floor.transform.localPosition = new Vector3((float)width / 2 - 0.5f, 0, (float)height / 2 - 0.5f);
+        width = 15;
+        height = 15;
+
+        //floor.transform.localScale = new Vector3((float)width / 10, 1, (float)height / 10);
+        floor.transform.localScale = new Vector3(width, 1, height);
+        //floor.transform.localPosition = new Vector3((float)width / 2 - 0.5f, 0, (float)height / 2 - 0.5f);
 
         timer = timeToUpdate;
 
         StartCoroutine(SendConfiguration());
     }
 
+    // Lo primero que se hace desde servidor
     IEnumerator SendConfiguration()
     {
         WWWForm form = new WWWForm();
 
         form.AddField("NAgents", NAgents.ToString());
-        form.AddField("width", width.ToString());
-        form.AddField("height", height.ToString());
+        form.AddField("width", (width * 10).ToString());
+        form.AddField("height", (height * 10).ToString());
 
         UnityWebRequest www = UnityWebRequest.Post(serverUrl + sendConfigEndpoint, form);
         www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -121,14 +125,107 @@ public class AgentController : MonoBehaviour
         {
             Debug.Log("Configuration upload complete!");
             Debug.Log("Getting Agents positions");
-            StartCoroutine(GetAgentsData());
-            StartCoroutine(GetObstacleData());
+            StartCoroutine(GetRobotsData());
+            // StartCoroutine(GetBoxData());
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GetRobotsData()
     {
-        
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getRobotsEndpoint);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else
+        {
+            robotsData = JsonUtility.FromJson<RobotsData>(www.downloadHandler.text);
+
+            foreach (RobotData rob in robotsData.positions)
+            {
+                Vector3 newAgentPosition = new Vector3(rob.x, rob.y, rob.z);
+
+                if (!started)
+                {
+                    prevPositions[rob.id] = newAgentPosition;
+                    robots[rob.id] = Instantiate(robot, newAgentPosition, Quaternion.identity);
+                }
+                else
+                {
+                    Vector3 currentPosition = new Vector3();
+                    if (currPositions.TryGetValue(rob.id, out currentPosition))
+                        prevPositions[rob.id] = currentPosition;
+                    currPositions[rob.id] = newAgentPosition;
+                }
+            }
+
+            updated = true;
+            if (!started) started = true;
+        }
+    }
+
+    /* IEnumerator GetBoxData() 
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getBoxesEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else 
+        {
+            boxData = JsonUtility.FromJson<RobotsData>(www.downloadHandler.text);
+
+            Debug.Log(obstacleData.positions);
+
+            foreach(AgentData obstacle in obstacleData.positions)
+            {
+                Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+            }
+        }
+    } */
+
+
+    private void Update() 
+    {
+        if(timer < 0)
+        {
+            timer = timeToUpdate;
+            updated = false;
+            StartCoroutine(UpdateSimulation());
+        }
+
+        if (updated)
+        {
+            timer -= Time.deltaTime;
+            dt = 1.0f - (timer / timeToUpdate);
+
+            foreach(var rob in currPositions)
+            {
+                Vector3 currentPosition = rob.Value;
+                Vector3 previousPosition = prevPositions[rob.Key];
+
+                Vector3 interpolated = Vector3.Lerp(previousPosition, currentPosition, dt);
+                Vector3 direction = currentPosition - interpolated;
+
+                robots[rob.Key].transform.localPosition = interpolated;
+                if(direction != Vector3.zero) robots[rob.Key].transform.rotation = Quaternion.LookRotation(direction);
+            }
+
+            // float t = (timer / timeToUpdate);
+            // dt = t * t * ( 3f - 2f*t);
+        }
+    }
+
+    IEnumerator UpdateSimulation()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + updateEndpoint);
+        yield return www.SendWebRequest();
+ 
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else 
+        {
+            StartCoroutine(GetRobotsData());
+        }
     }
 }
